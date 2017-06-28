64d2fc659c8121455415081f409b6af732f96e7c
Heramb Sawant <hes278@mail.usask.ca>
2017-06-27 23:24:17 -0600

    Finished register activity and UI, began signIn

--- app/src/main/java/com/example/heramb/applicationoutline/Interactions/InteractionsActivity.java
+++ app/src/main/java/com/example/heramb/applicationoutline/Interactions/InteractionsActivity.java
@@ -1,15 +1,23 @@
 package com.example.heramb.applicationoutline.Interactions;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.heramb.applicationoutline.R;
+import com.example.heramb.applicationoutline.Registration.RegisterActivity;
+import com.example.heramb.applicationoutline.Registration.SignInActivity;
 import com.example.heramb.applicationoutline.Utils.BottomNavigationViewHelper;
 import com.example.heramb.applicationoutline.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -19,16 +27,33 @@ public class InteractionsActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 0;
 
     private Context mContext = InteractionsActivity.this;
+    private Button signOut;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactions);
         Log.d(TAG, "onCreate: starting.");
+        signOut = (Button) findViewById(R.id.signOutButton);
 
         initImageLoader();
         setupBottomNavigationView();
+        setUpFirebaseAuth();
+
+        signOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(TAG, "Navigating to register activity");
+                FirebaseAuth.getInstance().signOut(); //End user session
+                startActivity(new Intent(mContext, SignInActivity.class)); //Go back to home page
+                finish();
+            }
+        });
     }
+
     private void initImageLoader(){
         UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
         ImageLoader.getInstance().init(universalImageLoader.getConfig());
@@ -45,5 +70,45 @@ public class InteractionsActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Firebase~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+    private void verifyCurrentUser(FirebaseUser user){
+        if(user == null){
+            Intent intent = new Intent(mContext, SignInActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void setUpFirebaseAuth(){
+        Log.d(TAG, "Setting up firebase authentication");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                verifyCurrentUser(user);
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- app/src/main/java/com/example/heramb/applicationoutline/Profile/SignOutFragment.java
+++ app/src/main/java/com/example/heramb/applicationoutline/Profile/SignOutFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,7 +17,7 @@ public class SignOutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_signout, container, false);
+        View view = inflater.inflate(R.layout.activity_sign_in, container, false);
 
         return view;
     }
