ca00cc7fa384f9343239da7aeac81b6ccb28ac1c
Heramb Sawant <hes278@mail.usask.ca>
2017-06-30 16:49:21 -0600

    Began implementing search functionality

--- app/src/main/java/com/example/heramb/applicationoutline/Search/SearchActivity.java
+++ app/src/main/java/com/example/heramb/applicationoutline/Search/SearchActivity.java
@@ -1,27 +1,71 @@
 package com.example.heramb.applicationoutline.Search;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.example.heramb.applicationoutline.R;
+import com.example.heramb.applicationoutline.Registration.SignInActivity;
 import com.example.heramb.applicationoutline.Utils.BottomNavigationViewHelper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
     private static final String TAG = "SearchActivity";
     private static final int ACTIVITY_NUM = 1;
 
-    private Context mContext = SearchActivity.this;
+    private Context mContext;
+    private Button submit;
+    private Spinner location;
+    private Spinner service;
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        mContext = SearchActivity.this;
 
+        setUpWidgets();
+        init();
         setupBottomNavigationView();
+        setUpFirebaseAuth();
+    }
+
+    private void setUpWidgets(){
+        submit = (Button) findViewById(R.id.searchSubmitButton);
+        location = (Spinner) findViewById(R.id.searchLocationSpinner);
+        service = (Spinner) findViewById(R.id.searchServiceSpinner);
+    }
+
+    private void init(){
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(TAG, "Navigating to register activity");
+                Intent intent = new Intent(mContext, SearchListActivity.class);
+                intent.putExtra("service",service.getSelectedItem().toString());
+                intent.putExtra("location",location.getSelectedItem().toString());
+                startActivity(intent);
+            }
+        });
     }
 
     /**
@@ -36,5 +80,44 @@ public class SearchActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Firebase~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+    private void verifyCurrentUser(FirebaseUser user){
+        if(user == null){
+            Intent intent = new Intent(mContext, SignInActivity.class);
+            startActivity(intent);
+        }
+    }
 
+    private void setUpFirebaseAuth(){
+        Log.d(TAG, "Setting up firebase authentication");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                verifyCurrentUser(user);
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
